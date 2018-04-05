518e559991a2f97dc7ce088acd6b70cdb2e91719
joshbrun <jbru239@aucklanduni.ac.nz>
2018-04-05 20:37:26 +1200

    Adding First video, simple json interaction

---
+++ YoutubeDownloader/YoutubeDownloader.py
@@ -0,0 +1,33 @@
+#Nethan do this.
+import json
+import os
+
+def downloadVideo(url, name):
+    '''
+    :param url: The url of the YouTube video.
+    :param name: The name of the file to save the YouTube video.
+    :return: None
+    '''
+    print("downloading %s from %s"%(name, url))
+
+def bulkDownloadVideos():
+    '''
+    :param videos: The array of videos to download
+    :return:
+    '''
+    # Read the json videos file
+    videos = json.load(open(os.getcwd()+'\YoutubeDownloader\YoutubeVideos.json'))['Videos']
+    print("Videos count: %d" % (len(videos)))
+    for video in videos:
+        url = video['url']
+        label = video['label']
+        exercise = video['exercise']
+        sex = video['sex']
+        view = video['view']
+        filename = "%s_%s_%s_%s.mp4" % (label, exercise, sex, view)
+        downloadVideo(url, filename)
+
+
+
+
+
---
+++ controller.py
@@ -0,0 +1,8 @@
+from YoutubeDownloader.YoutubeDownloader import bulkDownloadVideos
+
+#Does everything
+
+#Download the videos
+bulkDownloadVideos()
+
+#Split the videos into frames

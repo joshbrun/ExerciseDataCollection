f6c6adda320f926e7ef44a92abef149179b6cc88
Josh Brundan <joshbrun@users.noreply.github.com>
2018-05-24 23:30:12 +1200

    Changes to file and paths variables

--- controller.py
+++ controller.py
@@ -9,20 +9,26 @@ from videosplitter.videosplitter import split_videos
 from jsonprocessing.sortjsonfiles import sort_json_files
 from jsonprocessing.processjson import process_json
 from openpose.openPose import run_openpose
-import os
 
-import json
+from os.path import join
+from json import load
 
 # File and directory locations.
-data_dir = "data"
-videos_file = os.path.join(data_dir, "YoutubeVideos.json")
-openpose_input = os.path.join(data_dir, "frames")
-openpose_output = os.path.join(data_dir, "json")
-training_dir = os.path.join(data_dir, "training")
+DATA_DIR = "data"
+VIDEO_FILE = "YoutubeVideos.json"
+FRAMES_DIR = "frames"
+JSON_DIR = "json"
+TRAINING_DIR = "training"
+
+# Paths
+video_path = join(DATA_DIR, VIDEO_FILE)
+frames_path = join(DATA_DIR, FRAMES_DIR)
+json_path = join(DATA_DIR, JSON_DIR)
+training_path = join(DATA_DIR, JSON_DIR)
 
 # Sequential Pipeline flow
 # Gets the list of videos
-videos = json.load(open(videos_file))['Videos']
+videos = load(open(video_path))['Videos']
 
 # Download the videos
 bulk_download_videos(videos)
@@ -31,10 +37,10 @@ bulk_download_videos(videos)
 split_videos(videos)
 
 # This requires the bin, include, lib and models in the root dir
-run_openpose(openpose_input, openpose_output)
+run_openpose(frames_path, json_path)
 
 # Sort the Json files into directories
 sort_json_files()
 
 # Process the Json files into Trainable Normalized vectors
-process_json(data_dir, training_dir)
+process_json(DATA_DIR, training_path)

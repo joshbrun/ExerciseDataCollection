6c705eb4d1cf06326f3aa022af56338c528af3c1
Josh Brundan <joshbrun@users.noreply.github.com>
2018-05-24 14:46:23 +1200

    Uncommenting controller flow

--- controller.py
+++ controller.py
@@ -20,20 +20,21 @@ openpose_input = os.path.join(data_dir, "frames")
 openpose_output = os.path.join(data_dir, "json")
 training_dir = os.path.join(data_dir, "training")
 
+# Sequential Pipeline flow
 # Gets the list of videos
-# videos = json.load(open(videos_file))['Videos']
+videos = json.load(open(videos_file))['Videos']
 
 # Download the videos
-# bulk_download_videos(videos)
+bulk_download_videos(videos)
 
 # Split the videos into frames
-# split_videos(videos)
+split_videos(videos)
 
 # This requires the bin, include, lib and models in the root dir
-# run_openpose(openpose_input, openpose_output)
+run_openpose(openpose_input, openpose_output)
 
 # Sort the Json files into directories
-# sort_json_files()
+sort_json_files()
 
 # Process the Json files into Trainable Normalized vectors
 process_json(data_dir, training_dir)
